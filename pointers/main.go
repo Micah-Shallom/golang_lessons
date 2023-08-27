7c592bb53cf7dd01b94e193c8a6a879619d860a0
Micah <micahshallom@gmail.com>
2023-08-27 11:56:24 +0100

    finished up on pointer manipulations in golang

--- pointers/main.go
+++ pointers/main.go
@@ -2,12 +2,101 @@ package main
 
 import "fmt"
 
+func change(a *int) *float64 {
+	*a = 100
+
+	b := 5.5
+	return &b
+}
+
+func changeVar(a int) {
+	a = 66
+}
+
 func pointer_functions() {
 	x := 8
 	p := &x
+
+	fmt.Println("Value of x before calling change():", x)
+	change(p)
+	fmt.Println("Value of x after calling change():", x)
+
+	fmt.Println("Value of x before calling changeVar():", x)
+	changeVar(x)
+	fmt.Println("Value of x after calling changeVar():", x)
+
 }
 
+func changeValues(quantity int, price float64, title string, sold bool) {
+	quantity = 3
+	price = 500.4
+	title = "Mobile Phone"
+	sold = false
+}
+
+func changeValuesByPointer(quantity *int, price *float64, name *string, sold *bool){
+	*quantity = 3
+	*price = 500.5
+	*name = "Mobile Phone"
+	*sold = false
+}
+
+type Product struct {
+	price float64
+	productName string
+}
+
+func changeProduct(p Product){
+	p.price = 300
+	p.productName = "Bicycle"
+}
+func changeProductByPointer(p *Product){
+	(*p).price = 300 //equivalent to p.price = 300
+	(*p).productName = "Bicycle"
+}
+
+
+//testing the effect of pointers on maps and slice
+func changeSlice(s []int){
+	for i := range s{
+		s[i]++
+	}
+}
+
+func changeMap(m map[string]int){
+	m["a"] = 10
+	m["b"] = 20
+	m["c"] = 30
+
+} //we dont use pointers to maps cus a map value is already a pointer
+
 func main() {
+	quantity, price, title, sold := 5, 300.4, "Laptop", true
+	fmt.Println("BEFORE calling the changevalues():", quantity, price, title, sold)
+	changeValues(quantity, price, title, sold)
+	fmt.Println("AFTER calling the changevalues():", quantity, price, title, sold)
+	changeValuesByPointer(&quantity, &price, &title, &sold)
+	fmt.Println("AFTER calling the changevaluesByPointer():", quantity, price, title, sold)
+	
+
+	gift := Product{
+		price : 100,
+		productName: "Watch",
+	}
+	changeProduct(gift)
+	fmt.Println(gift)
+	changeProductByPointer(&gift)
+	fmt.Println(gift)
+
+	prices := []int{1,2,3}
+	changeSlice(prices)
+	fmt.Println("Prices slice after calling changeSlice():", prices)//when we check the output, we see that the function increments each value of the slice without the need of passing pointers to it, this is because a slice already contains a pointer to its backing array(main array)
+
+	myMap := map[string]int{"a": 1, "b": 2}
+	changeMap(myMap)
+	fmt.Println("myMap after calling changeMap():", myMap)
+
+
 	name := "Shallom"
 	fmt.Println(&name)
 
@@ -61,3 +150,6 @@ func main() {
 	pointer_functions()
 
 }
+
+
+//Note: In a nutshell int,float,strings,bools and structs are only modified by functions only if they are passed as pointers
