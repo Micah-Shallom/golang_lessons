ca93333b250073ddd17507a04de8889a159d5e2d
Micah <micahshallom@gmail.com>
2023-09-20 22:58:09 +0100

    multiple ways to run code

--- concurrency/http_serial2/main.go
+++ concurrency/http_serial2/main.go
@@ -2,44 +2,27 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"io/ioutil"
-	// "log"
 	"net/http"
 	"runtime"
 	"strings"
+	"time"
 )
 
-func checkAndSaveBody(url string, c chan string) {
+func checkURL(url string, c chan string) {
 	response, error := http.Get(url)
 
 	if error != nil {
 		s := fmt.Sprintf("%s - site is down!\n", url)
 		s += fmt.Sprintf("Error: %v \n", error)
-		c <- s //sending error into channel
-
-	} else {
-		defer response.Body.Close()
-		s := fmt.Sprintf("%s - status code is - %d \n", url, response.StatusCode)
-
-		if response.StatusCode == 200 {
-			bodyBytes, error := io.ReadAll(response.Body)
-
-			_ = error
-
-			file := strings.Split(url, "//")[1]
-			file += ".txt"
-			s := fmt.Sprintf("Writing response Body to %s\n", file)
-
-			error = ioutil.WriteFile(file, bodyBytes, 0664)
-			if error != nil {
-				// log.Fatal(error)
-				s += "Error writing file \n"
-				c <- s
-			}
-		}
-		s += fmt.Sprintf("%s - site is up", url)
-		c <- s
+		fmt.Println(s)
+		c <- url //sending error into channel
+		
+		} else {
+			defer response.Body.Close()
+			s := fmt.Sprintf("%s - status code is - %d \n", url, response.StatusCode)
+			s += fmt.Sprintf("%s - site is up", url)
+			fmt.Println(s)
+			c <- url
 
 	}
 }
@@ -52,14 +35,34 @@ func main() {
 	urls := []string{"http://www.google.com", "http://www.medium.com", "http://www.facebook.com"}
 
 	for _, url := range urls {
-		go checkAndSaveBody(url, ch)
-		fmt.Println(strings.Repeat("#", 20))
+		go checkURL(url, ch)
 	}
 
 	fmt.Println("No of Goroutines:", runtime.NumGoroutine())
 
-	for i := 0; i <= len(urls); i++ {
-		fmt.Println(<- ch)
+	// for i := 0; i <= len(urls); i++ {
+	// 	fmt.Println(<- ch)
+	// }
+
+	// for {
+	// 	go checkURL(<- ch, ch)
+	// 	fmt.Println(strings.Repeat("#", 30))
+	// 	time.Sleep(time.Second * 2)
+	// }
+	
+	// for url := range ch {
+	// 	go checkURL(url, ch)
+	// 	fmt.Println(strings.Repeat("#", 30))
+	// 	time.Sleep(time.Second * 2)
+	// }
+	
+	for url := range ch {
+		go func(u string){
+			checkURL(u, ch)
+			fmt.Println(strings.Repeat("#", 30))
+			time.Sleep(time.Second * 2)
+	
+		}(url)
 	}
 
 }
